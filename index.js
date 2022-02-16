a6c13ad5e59c5d8839150dc84438793c1ce458a2
July Moss <july.x.moss@gmail.com>
2022-02-16 14:57:59 +0000

    .env created

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+//why do we need to separate this part from app.js?
+
 const app = require('./src/app.js');
 
-const APP_PORT = 4000;
+const APP_PORT = process.env.PORT || 4000;
+console.log(process.env.PORT);
 
 app.listen(APP_PORT, () => {
     console.log(`App is listening on port ${APP_PORT}`);
